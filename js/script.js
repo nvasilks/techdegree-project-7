b311bce880fde9d06263227b78848bc094968447
nvasilks <61193591+nvasilks@users.noreply.github.com>
2020-05-09 21:25:27 +0100

    final-version

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-// Alert
+// alert
 
 const bell = document.querySelector(".bell-icon");
 const alertMain = document.querySelector("#alert");
@@ -10,22 +10,22 @@ const gDot = document.querySelector(".green-dot");
 bell.addEventListener('click', open => {alertMain.style.display = "flex"});
 closeAlert.addEventListener('click', clo => {alertMain.style.display = "none"; gDot.style.display = "none"; });
 
-// Message
+// message
 
 const user = document.getElementById("userField");
 const message = document.getElementById("messageField");
 const send = document.getElementById("send");
 
 send.addEventListener('click', () => {
-    // ensure user and message fields are filled out
+
     if (user.value === "" && message.value === "") {
-    alert("Please fill out user and message fields before sending");
+    alert("User and Message fields are required");
     } else if (user.value === "" ) {
-    alert("Please fill out user field before sending");
+    alert("User field is required");
     } else if (message.value === "" ) {
-    alert("Please fill out message field before sending");
+    alert("Message field is required");
     } else {
-    alert(`Message successfully sent to: ${user.value}`);
+    alert(`Your message has been sent to: ${user.value}`);
     }
     });
 
